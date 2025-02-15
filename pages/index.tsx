aea18977c7ab91ae02341afc1dcf313f204f2104
sasasanono <ukakau@gmail.com>
2025-02-15 11:24:26 +0900

    ログインボタン作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { signIn } from "next-auth/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,12 +38,7 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <button onClick={()=>signIn()}>
             <Image
               className="dark:invert"
               src="/vercel.svg"
@@ -50,8 +46,8 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+            ログイン
+          </button>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
@@ -92,21 +88,6 @@ export default function Home() {
             height={16}
           />
           Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
         </a>
       </footer>
     </div>
